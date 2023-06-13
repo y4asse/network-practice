26032208a543d869f6ce5163c54db2a67980378f
y4asse <yasse0218@icloud.com>
2023-06-14 02:56:28 +0900

    comment out

--- parse.c
+++ parse.c
@@ -81,13 +81,15 @@ void error(char *s)
 
 void outblock(void)
 {
+	// var 変数宣言の処理
 	if (tok.attr == RWORD && tok.value == VAR)
 	{
-		do
+		do // コンマになるまで繰り返す
 		{
 			getsym();
 			if (tok.attr == IDENTIFIER)
 			{
+				// 変数を記号表に保存する処理
 				s_table[tableIndex].addr = tableIndex;
 				strcpy(s_table[tableIndex].v, tok.charvalue);
 				fprintf(outfile, "loadi r0, 0\n");
@@ -98,7 +100,7 @@ void outblock(void)
 				error("Variable name is needed.");
 		} while (tok.attr == SYMBOL && tok.value == COMMA);
 
-		if (tok.attr == SYMBOL && tok.value == SEMICOLON)
+		if (tok.attr == SYMBOL && tok.value == SEMICOLON) // セミコロンの時終わる
 			getsym();
 		else
 			error("Semicolon is needed");
@@ -108,10 +110,11 @@ void outblock(void)
 
 void statement(void)
 {
-	rIndex = 0;
+	rIndex = 0; // レジスタのインデックス
 	if (tok.attr == IDENTIFIER)
 	{
-		int memoryAddress;
+		int memoryAddress; // メモリアドレスを一時的に保存する
+		// 逐次探索により，記号表から変数名に一致するものを取得する
 		for (int i = 0; i < sizeof(s_table) / sizeof s_table[0]; i++)
 		{
 			if (strcmp(s_table[i].v, tok.charvalue) == 0)
@@ -127,7 +130,7 @@ void statement(void)
 
 		getsym();
 
-		if (tok.attr == SYMBOL && tok.value == BECOMES)
+		if (tok.attr == SYMBOL && tok.value == BECOMES) //:=の時の処理
 		{
 			getsym();
 			expression();
@@ -136,15 +139,15 @@ void statement(void)
 		else
 			error("Assignment operator or Left parenthesis is needed.");
 	}
-	else if (tok.attr == RWORD && tok.value == BEGIN)
+	else if (tok.attr == RWORD && tok.value == BEGIN) // beginの時の処理
 	{
 		do
 		{
 			getsym();
 			statement();
-		} while (tok.attr == SYMBOL && tok.value == SEMICOLON);
+		} while (tok.attr == SYMBOL && tok.value == SEMICOLON); // セミコロンになるまで続ける
 
-		if (tok.attr == RWORD && tok.value == END)
+		if (tok.attr == RWORD && tok.value == END) // endになったら正常に終わる
 		{
 			getsym();
 		}
@@ -154,34 +157,34 @@ void statement(void)
 			error("End is needed.");
 		}
 	}
-	else if (tok.attr == RWORD && tok.value == IF)
+	else if (tok.attr == RWORD && tok.value == IF) // ifの時の処理
 	{
-		labelNumber += 2;
-		int localNumber = labelNumber;
+		labelNumber += 2;			   // 使用する文のラベルを加算する
+		int localNumber = labelNumber; // 一時的にラベルの番号を保存する
 		getsym();
 		condition();
 
-		if (tok.attr == RWORD && tok.value == THEN)
+		if (tok.attr == RWORD && tok.value == THEN) // thenの時の処理
 		{
 			getsym();
 			statement();
-			fprintf(outfile, "jmp L%d\n", localNumber - 1);
-			fprintf(outfile, "L%d:\n", localNumber - 2);
+			fprintf(outfile, "jmp L%d\n", localNumber - 1); // 先ほど保持しておいたラベルの番号を使用して，jmpを作成
+			fprintf(outfile, "L%d:\n", localNumber - 2);	// 先ほど保持しておいたラベルの番号を使用して，ifのラベルを作成
 
-			if (tok.attr == RWORD && tok.value == ELSE)
+			if (tok.attr == RWORD && tok.value == ELSE) // elseの時の処理
 			{
 				getsym();
 				statement();
 			}
-			fprintf(outfile, "L%d:\n", localNumber - 1);
+			fprintf(outfile, "L%d:\n", localNumber - 1); // 先ほど保持しておいたラベルの番号を使用して，ifのラベルを作成
 		}
 		else
 			error("Then is needed.");
 	}
-	else if (tok.attr == RWORD && tok.value == WHILE)
+	else if (tok.attr == RWORD && tok.value == WHILE) // whileの時
 	{
-		labelNumber += 2;
-		int localNumber = labelNumber;
+		labelNumber += 2;			   // 使用する文のラベルを加算する
+		int localNumber = labelNumber; // 一時的にラベルの番号を保存する
 		fprintf(outfile, "L%d:\n", localNumber - 1);
 		getsym();
 		condition();
@@ -190,19 +193,20 @@ void statement(void)
 		{
 			getsym();
 			statement();
-			fprintf(outfile, "jmp L%d\n", localNumber - 1);
-			fprintf(outfile, "L%d:\n", localNumber - 2);
+			fprintf(outfile, "jmp L%d\n", localNumber - 1); // 先ほど保持しておいたラベルの番号を使用して，jmpを作成
+			fprintf(outfile, "L%d:\n", localNumber - 2);	// 先ほど保持しておいたラベルの番号を使用して，ラベルを作成
 		}
 		else
 			error("Do is needed.");
 	}
-	else if (tok.attr == RWORD && tok.value == WRITE)
+	else if (tok.attr == RWORD && tok.value == WRITE) // writeの時の処理
 	{
 		do
 		{
 			getsym();
 			if (tok.attr == NUMBER)
 			{
+				// numberだったらそのままレジスタに入れて出力
 				fprintf(outfile, "loadi r0, %d\n", tok.value);
 				fprintf(outfile, "writed r0\n");
 				fprintf(outfile, "loadi r0,'\\n'\n");
@@ -211,6 +215,7 @@ void statement(void)
 			}
 			else if (tok.attr == IDENTIFIER)
 			{
+				// 変数だったら，記号表から逐次探索で合致するものを取得して，レジスタに格納して出力
 				for (int i = 0; i < sizeof(s_table) / sizeof s_table[0]; i++)
 				{
 					if (strcmp(s_table[i].v, tok.charvalue) == 0)
@@ -240,12 +245,9 @@ void statement(void)
 
 void factor(void)
 {
-	if (tok.attr == SYMBOL && tok.value == MINUS)
+	if (tok.attr == IDENTIFIER) // 変数の時の処理
 	{
-		getsym();
-	}
-	if (tok.attr == IDENTIFIER)
-	{
+		// 逐次探索で記号表から合致する変数の値を取得
 		for (int i = 0; i < sizeof(s_table) / sizeof s_table[0]; i++)
 		{
 			if (strcmp(s_table[i].v, tok.charvalue) == 0)
@@ -258,24 +260,25 @@ void factor(void)
 				error("Undeclared variable is used");
 			}
 		}
-		rIndex++;
+		rIndex++; // レジスタの番号を1個ずらす
 		getsym();
 	}
 	else if (tok.attr == NUMBER)
 	{
+		// 数字が16bitを超えるときの処理
 		if (tok.value > 32767 || tok.value < -32767)
 		{
-			dataLabelNumber[dataLabelIndex] = tok.value;
+			dataLabelNumber[dataLabelIndex] = tok.value; // dataLabelNumberという変数にデータを保存し，あとで，ラベルとして出力する
 			fprintf(outfile, "load r%d, Data%d\n", rIndex, dataLabelIndex++);
 		}
 		else
 		{
 			fprintf(outfile, "loadi r%d, %d\n", rIndex, tok.value);
 		}
-		rIndex++;
+		rIndex++; // レジスタの番号を1個ずらす
 		getsym();
 	}
-	else if (tok.attr == SYMBOL && tok.value == LPAREN)
+	else if (tok.attr == SYMBOL && tok.value == LPAREN) //(の時の処理
 	{
 		getsym();
 		expression();
@@ -297,55 +300,55 @@ void factor(void)
 void term(void)
 {
 	factor();
-	while ((tok.attr == SYMBOL && tok.value == TIMES) || (tok.attr == RWORD && tok.value == DIV))
+	while ((tok.attr == SYMBOL && tok.value == TIMES) || (tok.attr == RWORD && tok.value == DIV)) //*か，divの間繰り返す
 	{
-		if (tok.value == TIMES)
+		if (tok.value == TIMES) //*の時の処理
 		{
 			getsym();
 			factor();
 			fprintf(outfile, "mulr r%d, r%d\n", rIndex - 2, rIndex - 1);
 		}
-		else
+		else // divの時の処理
 		{
 			getsym();
 			factor();
 			fprintf(outfile, "divr r%d, r%d\n", rIndex - 2, rIndex - 1);
 		}
-		rIndex--;
+		rIndex--; // ずらしていたレジスタインデックスを元に戻す
 	}
 }
 
 void expression(void)
 {
 	term();
-	while ((tok.attr == SYMBOL && tok.value == PLUS) || (tok.attr == SYMBOL && tok.value == MINUS))
+	while ((tok.attr == SYMBOL && tok.value == PLUS) || (tok.attr == SYMBOL && tok.value == MINUS)) //+か-の間繰り返す
 	{
-		if (tok.value == PLUS)
+		if (tok.value == PLUS) //+の時の処理
 		{
 			getsym();
 			term();
 			fprintf(outfile, "addr r%d, r%d\n", rIndex - 2, rIndex - 1);
 		}
-		else
+		else //-の時の処理
 		{
 			getsym();
 			term();
 			fprintf(outfile, "subr r%d, r%d\n", rIndex - 2, rIndex - 1);
 		}
-		rIndex--;
+		rIndex--; // ずらしていたレジスタインデックスを元に戻す
 	}
 }
 
 void condition(void)
 {
-	rIndex = 0;
+	rIndex = 0; // expressionに入る前にレジスタの番号を初期化する
 	expression();
 	fprintf(outfile, "loadi r1, 0\n");
-	fprintf(outfile, "addr r1, r0\n");
+	fprintf(outfile, "addr r1, r0\n"); // expressionでr0に入っていた値をとりだす
 	if (tok.attr == SYMBOL)
 	{
-		char comp[10];
-		switch (tok.value)
+		char comp[10];	   // 一時的に文字列を保存しておく変数
+		switch (tok.value) // strcpyを使って，文字列をcompにコピーしておく
 		{
 		case LESSTHAN:
 			strcpy(comp, "jge");
@@ -376,8 +379,8 @@ void condition(void)
 			break;
 		}
 		expression();
-		fprintf(outfile, "cmpr r0, r1\n");
-		fprintf(outfile, "%s L%d\n", comp, labelNumber - 2);
+		fprintf(outfile, "cmpr r0, r1\n");					 // cmprを実行する
+		fprintf(outfile, "%s L%d\n", comp, labelNumber - 2); // ラベルナンバーを設定する
 	}
 	else
 		error("Relational operator is needed.");
